f821f13bc00a7cf78c2c4c36c57d9198ec907686
laskiewicz <laskiewicz@outlook.com>
2021-03-15 16:52:39 +0100

    Added Win2D Canvas

--- WinUITheGameOfLife/WinUITheGameOfLife/Views/BoardView.xaml.cs
+++ WinUITheGameOfLife/WinUITheGameOfLife/Views/BoardView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.UI;
 using Microsoft.UI.Xaml.Controls;
 using WinUITheGameOfLife.ViewModels;
 
@@ -16,5 +18,11 @@ namespace WinUITheGameOfLife.Views
             ViewModel = Ioc.Default.GetService<BoardViewModel>();
         }
         public BoardViewModel ViewModel { get; set; }
+
+        void canvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawEllipse(155, 115, 80, 30, Colors.Black, 3);
+            args.DrawingSession.DrawText("Hello, world!", 100, 100, Colors.Yellow);
+        }
     }
 }
